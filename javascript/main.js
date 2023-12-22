b83eecaaf323f53ee248ddd1f666ea64ec772598
suhyun_si <134688148+suhyun7512@users.noreply.github.com>
2023-12-22 23:27:00 +0900

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    ul('0');
+    //updateRankings('rank-views');
+
+});
+
+
+
+window.onload = function() {
+    handleClick1(this, 'ma-content-custom'),
+    handleClick(this, 'webtoon-site-1'),playGif1();
+};
